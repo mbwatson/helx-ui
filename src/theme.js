6f15ce359c0ea97ac533f4e278cf0123b82a86e5
Matt Watson <mattwat@gmail.com>
2020-11-03 12:02:44 -0500

    add colors to theme

--- src/theme.js
+++ src/theme.js
@@ -1,41 +1,38 @@
 export const theme = {
-    color: {
-        primary: {
-            light: '#32daf6',
-            main: '#02aac6',
-            dark: '#028aa6',
-        },
-        extended: {
-            mist: '#d9ecf6',
-            moss: '#5e9b8b',
-            ocean: '#58a3d2',
-            sherbet: '#ffa872',
-        },
-        black: '#181818',
-        grey: {
-            light: '#ddd',
-            main: '#999',
-            dark: '#333',
-        },
-        white: '#f1f2f3',
-        success: '#7ebb9b',
-        info: '#49a0d9',
-        warning: '#df9852',
-        danger: '#c16a5d',
+  color: {
+    primary: {
+      light: '#30d8f1',
+      main: '#00a8c1',
+      dark: '#007891',
     },
-    border: {
-        radius: '3px',
-        width: '1px',
+    extended: {
+      crimson: '#9e0817',
     },
-    spacing: {
-        xs: '0.25rem', extraSmall: '0.25rem',
-        sm: '0.5rem', small: '0.5rem',
-        md: '1rem', medium: '1rem',
-        lg: '2rem', large: '2rem',
-        xl: '3rem', extraLarge: '3rem',
+    black: '#181818',
+    grey: {
+      light: '#ddd',
+      main: '#999',
+      dark: '#333',
     },
-    debug: `
-        border: 1px solid #f99;
-        * { border: 1px solid #f99; }
-    `
+    white: '#f1f2f3',
+    success: '#7ebb9b',
+    info: '#49a0d9',
+    warning: '#df9852',
+    danger: '#c16a5d',
+  },
+  border: {
+    radius: '3px',
+    width: '1px',
+  },
+  spacing: {
+    xs: '0.25rem', extraSmall: '0.25rem',
+    sm: '0.5rem', small: '0.5rem',
+    md: '1rem', medium: '1rem',
+    lg: '2rem', large: '2rem',
+    xl: '3rem', extraLarge: '3rem',
+  },
+  debug: `
+    border: 1px solid #f99;
+    * { border: 1px solid #f99; }
+  `
 }
