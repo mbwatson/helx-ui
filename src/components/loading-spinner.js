bfd27aa0453c9e555572ead49f04923819195e10
Matt Watson <mattwat@gmail.com>
2020-11-05 00:05:45 -0500

    redesign loading-spinner component

--- src/components/loading-spinner.js
+++ src/components/loading-spinner.js
@@ -1,90 +1,54 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled, { keyframes } from 'styled-components'
-
-const throb = keyframes`
-    0% {
-        opacity: 0;
-        transform: scale(1) rotate(180deg);
-    }
-    33.6% {
-        transform: scale(1.3) rotate(180deg);
-    }
-    42.2% {
-        transform: scale(0.4) rotate(180deg);
-    }
-    50% {
-        opacity: 1;
-        transform: scale(1.5) rotate(90deg);
-    }
-    100% {
-        opacity: 0;
-        transform: scale(1) rotate(0deg);
-    }
-`
-
-const glow = keyframes`
-    0% {
-        opacity: 0.45;
-    }
-    50% {
-        opacity: 0.9;
-    }
-    100% {
-        opacity: 0.45;
-    }
-`
+import styled, { keyframes, useTheme } from 'styled-components'
 
 const Wrapper = styled.div`
-    // & * {border: 1px solid #f99;}
-    padding: 1rem;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
+  display: flex;
+  justify-content: center;
 `
 
-const Tray = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
+const revolve = keyframes`
+  0% {
+    transform: rotate(0deg) scale(1);
+    opacity: 0.5;
+  }
+  50% {
+    transform: rotate(180deg) scale(2);
+    opacity: 1;
+  }
+  100% {
+    transform: rotate(360deg) scale(1);
+    opacity: 0.5;
+  }
 `
 
-const Dot = styled.div`
-    background-color: ${ props => props.color || '#333' };
-    margin: 0.75rem;
-    height: 0.75rem;
-    width: 0.75rem;
-    animation: ${ throb } 1500ms ease-out infinite;
-    &:nth-child(1) { animation-delay: 0ms; }
-    &:nth-child(2) { animation-delay: 75ms; }
-    &:nth-child(3) { animation-delay: 150ms; }
-    &:nth-child(4) { animation-delay: 225ms; }
-    position: relative;
+const Orb = styled.circle`
+  transform-origin: center center;
+  animation-name: ${ revolve };
+  animation-duration: 1000ms;
+  animation-iteration-count: infinite;
+  animation-direction: normal;
+  animation-timing-function: linear; /* ease, ease-in, ease-in-out, ease-out, linear, cubic-bezier(x1, y1, x2, y2) */
+  animation-fill-mode: forwards;
+  animation-delay: 0;
 `
 
-const LoadingText = styled.div`
-    text-align: center;
-    color: ${ props => props.color || '#333' };
-    opacity: 0.75;
-    animation: ${ glow } 1500ms ease-out infinite;
-    padding: 0.5rem;
-`
-
-export const LoadingSpinner = ({ text = "", textPlacement, color }) => (
+export const LoadingSpinner = ({ color, ...rest }) => {
+  const theme = useTheme()
+  const fill = color ? color : theme.color.primary.main
+  return (
     <Wrapper>
-        { text !== '' && textPlacement ==='top' && <LoadingText color={ color }>{ text }</LoadingText> }
-        <Tray>
-            <Dot color={ color } />
-            <Dot color={ color } />
-            <Dot color={ color } />
-            <Dot color={ color } />
-        </Tray>
-        { text !== '' && textPlacement ==='bottom' && <LoadingText color={ color }>{ text }</LoadingText> }
+      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" fill={ color }
+        x="0px" y="0px" width="60px" height="60px" viewBox="0 0 60 60"
+        { ...rest }
+      >
+        <Orb cx="30" cy="20" r="3" fill={ fill } />
+        <Orb cx="30" cy="40" r="3" fill={ fill } />
+      </svg>
     </Wrapper>
-)
+  )
+}
 
 LoadingSpinner.propTypes = {
-    text: PropTypes.string,
-    textPlacement: PropTypes.oneOf(['top', 'bottom']),
-    color: PropTypes.string,
+  color: PropTypes.string,
 }
